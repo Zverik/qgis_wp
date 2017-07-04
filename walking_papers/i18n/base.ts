3c65af831f5762eb3cc260c0f7fdce7012d1e5ed
Ilya Zverev <zverik@textual.ru>
2017-07-05 00:54:35 +0300

    Update translations

--- walking_papers/i18n/base.ts
+++ walking_papers/i18n/base.ts
@@ -3,163 +3,163 @@
 <context>
     <name>WalkingPapersPlugin</name>
     <message>
-        <location filename="mainplugin.py" line="127"/>
+        <location filename="mainplugin.py" line="122"/>
         <source>Walking Papers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="358"/>
+        <location filename="mainplugin.py" line="372"/>
         <source>Download OSM Data</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="89"/>
+        <location filename="mainplugin.py" line="91"/>
         <source>Downloads data from OpenStreetMap and styles it.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="332"/>
+        <location filename="mainplugin.py" line="346"/>
         <source>Open OSM Data</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="96"/>
+        <location filename="mainplugin.py" line="98"/>
         <source>Converts OSM data, loads and styles it for walking papers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="101"/>
+        <location filename="mainplugin.py" line="103"/>
         <source>Create Pie Layers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="103"/>
+        <location filename="mainplugin.py" line="105"/>
         <source>Creates pie sheets and pie overview layers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Calculate Pie Rotation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="112"/>
-        <source>Adds or updates a rotation column with degrees. Requires a pie sheets layer</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Prepare Atlas</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="119"/>
+        <location filename="mainplugin.py" line="114"/>
         <source>Creates an atlas in map composer to print walking papers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="177"/>
+        <location filename="mainplugin.py" line="135"/>
         <source>No layer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="177"/>
+        <location filename="mainplugin.py" line="135"/>
         <source>Please add &quot;{}&quot; layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="145"/>
+        <location filename="mainplugin.py" line="140"/>
         <source>No data</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="145"/>
+        <location filename="mainplugin.py" line="140"/>
         <source>No features in the &quot;{}&quot; layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="218"/>
+        <location filename="mainplugin.py" line="211"/>
         <source>Access error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="160"/>
+        <location filename="mainplugin.py" line="149"/>
         <source>Failed to load a temporary processing layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Done</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="171"/>
-        <source>Pie rotation values were updated.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainplugin.py" line="218"/>
+        <location filename="mainplugin.py" line="211"/>
         <source>Failed to add a &quot;{}&quot; field to the &quot;{}&quot; layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Now sketch pie on the &quot;{}&quot; layer and then split it into rectangle sheets on the &quot;{}&quot; layer. After that choose &quot;{}&quot; and then &quot;{}&quot;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="281"/>
+        <location filename="mainplugin.py" line="295"/>
         <source>Select GeoPackage file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="281"/>
+        <location filename="mainplugin.py" line="295"/>
         <source>GeoPackage File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="300"/>
+        <location filename="mainplugin.py" line="314"/>
         <source>Select OpenStreetMap file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="300"/>
+        <location filename="mainplugin.py" line="314"/>
         <source>OSM or GeoPackage File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="328"/>
+        <location filename="mainplugin.py" line="342"/>
         <source>Error running ogr2ogr: {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="332"/>
+        <location filename="mainplugin.py" line="346"/>
         <source>Error converting OSM to GeoPackage.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="358"/>
+        <location filename="mainplugin.py" line="372"/>
         <source>Draw a polygon in the &quot;{}&quot; layer and the choose the same menu item to download object in the polygon.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="367"/>
+        <location filename="mainplugin.py" line="381"/>
         <source>Select OpenStreetMap file to write</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="367"/>
+        <location filename="mainplugin.py" line="381"/>
         <source>OSM File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="400"/>
+        <location filename="mainplugin.py" line="414"/>
         <source>Download error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="400"/>
+        <location filename="mainplugin.py" line="414"/>
         <source>Failed to download data from Overpass API. See log for details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainplugin.py" line="223"/>
+        <source>Projection Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainplugin.py" line="223"/>
+        <source>Your project&apos;s map projection may not preserve angles. This is bad for understanding the map in walking papers. Do you want to change it to EPSG:3857?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- walking_papers/i18n/ru.ts
+++ walking_papers/i18n/ru.ts
@@ -1,166 +1,167 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="ru" sourcelanguage="en">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru" sourcelanguage="en">
 <context>
     <name>WalkingPapersPlugin</name>
     <message>
-        <location filename="mainplugin.py" line="127"/>
+        <location filename="mainplugin.py" line="122"/>
         <source>Walking Papers</source>
         <translation>Обходные листы</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="358"/>
+        <location filename="mainplugin.py" line="372"/>
         <source>Download OSM Data</source>
         <translation>Скачать данные OSM</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="89"/>
+        <location filename="mainplugin.py" line="91"/>
         <source>Downloads data from OpenStreetMap and styles it.</source>
         <translation>Загружает данные из базы OpenStreetMap и оформляет их.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="332"/>
+        <location filename="mainplugin.py" line="346"/>
         <source>Open OSM Data</source>
         <translation>Открыть файл OSM</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="96"/>
+        <location filename="mainplugin.py" line="98"/>
         <source>Converts OSM data, loads and styles it for walking papers</source>
         <translation>Преобразует файл OSM, загружает и оформляет слои в обходные листы</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="101"/>
+        <location filename="mainplugin.py" line="103"/>
         <source>Create Pie Layers</source>
         <translation>Создать слои пирога</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="103"/>
+        <location filename="mainplugin.py" line="105"/>
         <source>Creates pie sheets and pie overview layers</source>
         <translation>Добавляет слои для обзорного и полистового пирогов</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Calculate Pie Rotation</source>
         <translation>Вычислить повороты листов</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="112"/>
-        <source>Adds or updates a rotation column with degrees. Requires a pie sheets layer</source>
-        <translation>Добавляет или обновляет столбец со значениями в градусах. Требует слой пирога</translation>
-    </message>
-    <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Prepare Atlas</source>
         <translation>Подготовить к печати</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="119"/>
+        <location filename="mainplugin.py" line="114"/>
         <source>Creates an atlas in map composer to print walking papers</source>
         <translation>Создаёт атлас в редакторе макета, чтобы печатать обходные листы</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="177"/>
+        <location filename="mainplugin.py" line="135"/>
         <source>No layer</source>
         <translation>Нет слоя</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="177"/>
+        <location filename="mainplugin.py" line="135"/>
         <source>Please add &quot;{}&quot; layer.</source>
         <translation>Добавьте слой &quot;{}&quot;.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="145"/>
+        <location filename="mainplugin.py" line="140"/>
         <source>No data</source>
         <translation>Нет данных</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="145"/>
+        <location filename="mainplugin.py" line="140"/>
         <source>No features in the &quot;{}&quot; layer.</source>
         <translation>Нет объектов в слое &quot;{}&quot;.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="218"/>
+        <location filename="mainplugin.py" line="211"/>
         <source>Access error</source>
         <translation>Ошибка доступа</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="160"/>
+        <location filename="mainplugin.py" line="149"/>
         <source>Failed to load a temporary processing layer.</source>
         <translation>Не удалось загрузить временный слой.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Done</source>
         <translation>Готово</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="171"/>
-        <source>Pie rotation values were updated.</source>
-        <translation>Значения поворотов листов обновлены.</translation>
-    </message>
-    <message>
-        <location filename="mainplugin.py" line="218"/>
+        <location filename="mainplugin.py" line="211"/>
         <source>Failed to add a &quot;{}&quot; field to the &quot;{}&quot; layer.</source>
         <translation>Не удалось добавить поле &quot;{}&quot; в слой &quot;{}&quot;.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="270"/>
+        <location filename="mainplugin.py" line="284"/>
         <source>Now sketch pie on the &quot;{}&quot; layer and then split it into rectangle sheets on the &quot;{}&quot; layer. After that choose &quot;{}&quot; and then &quot;{}&quot;.</source>
         <translation>Теперь наметьте пирог в слое &quot;{}&quot; и затем разбейте его на прямоугольные листы в слое &quot;{}&quot;. После этого выберите &quot;{}&quot; и затем &quot;{}&quot;.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="281"/>
+        <location filename="mainplugin.py" line="295"/>
         <source>Select GeoPackage file</source>
         <translation>Выберите файл GeoPackage</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="281"/>
+        <location filename="mainplugin.py" line="295"/>
         <source>GeoPackage File</source>
         <translation>Файл GeoPackage</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="300"/>
+        <location filename="mainplugin.py" line="314"/>
         <source>Select OpenStreetMap file</source>
         <translation>Выберите файл OpenStreetMap</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="300"/>
+        <location filename="mainplugin.py" line="314"/>
         <source>OSM or GeoPackage File</source>
         <translation>Файл OSM или GeoPackage</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="328"/>
+        <location filename="mainplugin.py" line="342"/>
         <source>Error running ogr2ogr: {}</source>
         <translation>Ошибка вызова ogr2ogr: {}</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="332"/>
+        <location filename="mainplugin.py" line="346"/>
         <source>Error converting OSM to GeoPackage.</source>
         <translation>Не удалось сделать файл GeoPackage из OSM.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="358"/>
+        <location filename="mainplugin.py" line="372"/>
         <source>Draw a polygon in the &quot;{}&quot; layer and the choose the same menu item to download object in the polygon.</source>
         <translation>Нарисуйте полигон в слое &quot;{}&quot; и затем выберите тот же самый пункт меню для скачивания данных внутри полигона.</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="367"/>
+        <location filename="mainplugin.py" line="381"/>
         <source>Select OpenStreetMap file to write</source>
         <translation>Выберите файл OSM для сохранения</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="367"/>
+        <location filename="mainplugin.py" line="381"/>
         <source>OSM File</source>
         <translation>Файл OSM</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="400"/>
+        <location filename="mainplugin.py" line="414"/>
         <source>Download error</source>
         <translation>Ошибка скачивания</translation>
     </message>
     <message>
-        <location filename="mainplugin.py" line="400"/>
+        <location filename="mainplugin.py" line="414"/>
         <source>Failed to download data from Overpass API. See log for details.</source>
         <translation>Не удалось скачать данные из Overpass API. Ошибка записана в лог.</translation>
+    </message>
+    <message>
+        <location filename="mainplugin.py" line="223"/>
+        <source>Projection Warning</source>
+        <translation>Проблема с проекцией</translation>
+    </message>
+    <message>
+        <location filename="mainplugin.py" line="223"/>
+        <source>Your project&apos;s map projection may not preserve angles. This is bad for understanding the map in walking papers. Do you want to change it to EPSG:3857?</source>
+        <translation>Проекция вашего проекта может не сохранять углы. Это усложнит понимание карты в обходных листах. Поменять проекцию на EPSG:3857?</translation>
     </message>
 </context>
 </TS>
